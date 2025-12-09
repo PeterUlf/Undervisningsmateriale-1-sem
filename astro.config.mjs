28568c489e3691075c8f440336b47a516d729903
Peter Ulf <peuj@kea.dk>
2025-12-09 14:27:39 +0100

    scrum under projektstyring

--- astro.config.mjs
+++ astro.config.mjs
@@ -58,6 +58,21 @@ export default defineConfig({
           ],
         },
         {
+          label: "Projektstyring",
+          collapsed: true,
+          items: [
+            {
+              label: "Scrum Grundprincipper",
+              slug: "git/scrum",
+              badge: {
+                text: "Tema 5",
+                variant: "tip",
+                class: "badge-theme-5",
+              },
+            },
+          ],
+        },
+        {
           label: "Video / Foto",
           collapsed: true,
           items: [
