8bd4c76e09ffe7adacaca4d5ed0269e13d71bd55
Peter Ulf <peuj@kea.dk>
2026-02-04 17:29:43 +0100

    version og semester

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ const isDev = process.env.NODE_ENV !== "production";
 export default defineConfig({
   integrations: [
     starlight({
-      title: "MMD undervisningsmateriale E2025 V  0.4",
+      title: "MMD undervisningsmateriale F2026 V  0.5",
       customCss: ["./src/styles/custom.css"],
       components: {
         PageTitle: "./src/components/PageTitleWrapper.astro",
